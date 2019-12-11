0b7d5ecf8e269289e8f7ff3157421f2442519c94
Konrad Sobczak <k.sobczak@samsung.com>
2019-12-11 12:19:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/theforumapp/StartActivity.java
@@ -0,0 +1,46 @@
+package com.example.theforumapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button login;
+    Button register;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        login = findViewById(R.id.login_start);
+        register = findViewById(R.id.register_start);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, RegistrationActivity.class));
+            }
+        });
+    }
+}
